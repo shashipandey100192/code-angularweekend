d0cfd71b99c304bede5cb3a4224995006ddf1065
shashiweb <shashiweb64@gmail.com>
2024-06-22 12:48:46 +0530

    change

--- src/app/modules/auth/loginpage/loginpage.component.ts
+++ src/app/modules/auth/loginpage/loginpage.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 
 @Component({
   selector: 'app-loginpage',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,MatGridListModule,FormsModule,ReactiveFormsModule],
   templateUrl: './loginpage.component.html',
   styleUrl: './loginpage.component.scss'
 })
 export class LoginpageComponent {
 
+  tiles: Tile[] = [
+    {text: 'One', cols: 6, rows: 1, color: 'lightblue'},
+
+  ];
 }
--- src/app/modules/auth/registor/registor.component.ts
+++ src/app/modules/auth/registor/registor.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FormsModule,ReactiveFormsModule,FormControl,FormGroup, ɵFormControlCtor } from '@angular/forms';
 
 @Component({
   selector: 'app-registor',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,FormsModule,ReactiveFormsModule,FormControl,FormGroup],
   templateUrl: './registor.component.html',
   styleUrl: './registor.component.scss'
 })
 export class RegistorComponent {
+  
+
+  // registor = new FormGroup({
+  //     email:new FormControl('')
+  // })
+
+  // mysubmit()
+  // {
+  //   console.log(this.registor);
+  // }
+
+
 
 }
+
