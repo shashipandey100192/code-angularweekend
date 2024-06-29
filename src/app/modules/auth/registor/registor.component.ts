1c66f667f58a553a970f8d5a36ee15c33c226e65
shashiweb <shashiweb64@gmail.com>
2024-06-29 12:22:17 +0530

    change

--- src/app/modules/auth/registor/registor.component.ts
+++ src/app/modules/auth/registor/registor.component.ts
@@ -1,16 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { FormsModule,ReactiveFormsModule,FormControl,FormGroup} from '@angular/forms';
+import {ReactiveFormsModule,FormControl,FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-registor',
   standalone: true,
-  imports: [RouterLink,FormsModule,ReactiveFormsModule,FormControl,FormGroup],
+  imports: [RouterLink,ReactiveFormsModule],
   templateUrl: './registor.component.html',
   styleUrl: './registor.component.scss'
 })
 export class RegistorComponent implements OnInit {
   
+  myform = new FormGroup({
+    email: new FormControl(''),
+    fullname: new FormControl('',[Validators.required,Validators.minLength(5),Validators.maxLength(10)]),
+    phone : new FormControl(''),
+    course : new FormControl('')
+  })
+
+  myformsubmit()
+  {
+    console.log(this.myform);
+  }
 
 
 ngOnInit(): void {
